1fe79a5716c9687900e7b92fdc5c84a0b2681528
Jauhari, Rahul <Rahul.Jauhari@amd.com>
2026-01-21 15:11:35 +0530

    Add unit test for MatMul fusions (#44)
    - Add unit tests covering matmul operations fused with various
      activations and bias combinations:
    - matmul + relu
    - matmul + bias_add
    - matmul + bias_add + relu
    - matmul + bias + sigmoid
    - matmul + bias + tanh
    - matmul + bias + gelu (exact and approximate)
    - ZENAI-2706
    
    Signed-off-by: Rahul Jauhari <rjauhari@amd.com>
    Co-authored-by: Rahul Jauhari <rjauhari@amd.com>

---
+++ tests/matmul_bias_add_relu.py
@@ -0,0 +1,34 @@
+# ******************************************************************************
+# Copyright (c) 2023-2026 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ******************************************************************************
+
+import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
+a = tf.random.normal(shape=[24, 8], dtype=tf.float32, seed=5)
+b = tf.random.normal(shape=[8, 24], dtype=tf.float32, seed=5)
+bias = tf.random.normal(shape=[24], dtype=tf.float32, seed=5)
+
+with tf.device("/CPU:0"):
+  c = tf.matmul(a=a, b=b)
+  output = tf.nn.bias_add(c, bias)
+  output = tf.nn.relu(output)
+
+sess = tf.compat.v1.Session(
+    config=tf.compat.v1.ConfigProto(
+        allow_soft_placement=False,
+        log_device_placement=True))
+
+print(sess.run(output))
